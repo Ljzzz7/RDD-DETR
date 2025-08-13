190e0e739c8e38ced196b2c561aeab6417ad58e1
Ljz <65344909+Ljzzz7@users.noreply.github.com>
2025-08-13 16:22:12 +0800

    Create block.py

---
+++ modules/block.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from collections import OrderedDict
+
+from .conv import Conv, RepConv
+from .transformer import TransformerBlock
+__all__ = ('C3','RepC3', 'ConvNormLayer', 'BasicBlock', 'BottleNeck', 'Blocks')
+
+
+class RepC3(nn.Module):
+    """Rep C3."""
+
+    def __init__(self, c1, c2, n=3, e=1.0):
+        """Initialize CSP Bottleneck with a single convolution using input channels, output channels, and number."""
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.m = nn.Sequential(*[RepConv(c_, c_) for _ in range(n)])
+        self.cv3 = Conv(c_, c2, 1, 1) if c_ != c2 else nn.Identity()
+
+    def forward(self, x):
+        """Forward pass of RT-DETR neck layer."""
+        return self.cv3(self.m(self.cv1(x)) + self.cv2(x))
