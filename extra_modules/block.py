56399bd8e0b340014ea90baecbd7ceed0351c91f
Ljz <65344909+Ljzzz7@users.noreply.github.com>
2025-08-13 16:24:13 +0800

    Create block.py

---
+++ extra_modules/block.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.checkpoint as checkpoint
+import math
+import numpy as np
+
+__all__ = [  'gConvC3']
+
+class gConvBlock(nn.Module):
+	def __init__(self, dim, kernel_size=3, gate_act=nn.Sigmoid, net_depth=8):
+		super().__init__()
+		self.dim = dim
+
+		self.net_depth = net_depth
+		self.kernel_size = kernel_size
+
+		self.Wv = nn.Sequential(
+			nn.Conv2d(dim, dim, 1),
+			nn.Conv2d(dim, dim, kernel_size=kernel_size, padding=kernel_size//2, groups=dim, padding_mode='reflect')
+		)
+
+		self.Wg = nn.Sequential(
+			nn.Conv2d(dim, dim, 1),
+			gate_act() if gate_act in [nn.Sigmoid, nn.Tanh] else gate_act(inplace=True)
+		)
+
+		self.proj = nn.Conv2d(dim, dim, 1)
+
+		self.apply(self._init_weights)
+
+	def _init_weights(self, m):
+		if isinstance(m, nn.Conv2d):
+			gain = (8 * self.net_depth) ** (-1/4)    # self.net_depth ** (-1/2), the deviation seems to be too small, a bigger one may be better
+			fan_in, fan_out = nn.init._calculate_fan_in_and_fan_out(m.weight)
+			std = gain * math.sqrt(2.0 / float(fan_in + fan_out))
+			trunc_normal_(m.weight, std=std)
+
+			if m.bias is not None:
+				nn.init.constant_(m.bias, 0)
+
+	def forward(self, X):
+		out = self.Wv(X) * self.Wg(X)
+		out = self.proj(out)
+		return out
+
+
+class gConvC3(RepC3):
+    def __init__(self, c1, c2, n=3, e=1):
+        super().__init__(c1, c2, n, e)
+        c_ = int(c2 * e)  # hidden channels
+        self.m = nn.Sequential(*[gConvBlock(c_) for _ in range(n)])
